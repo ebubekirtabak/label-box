0300c479108317b0d7d7084639fc671d48800afe
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-02-28 21:29:44 +0300

    crash reporter added.

---
+++ utils/crash-reporter.js
@@ -0,0 +1,8 @@
+const { crashReporter } = require('electron')
+
+crashReporter.start({
+  productName: 'LabelBox',
+  companyName: 'ebubekirtabak',
+  submitURL: 'https://your-domain.com/url-to-submit',
+  uploadToServer: true
+})
