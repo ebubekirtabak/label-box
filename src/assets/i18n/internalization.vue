0d7e5cdfc0e02ec3d37bc07bd6d7db4f17b4ce0b
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-01 22:22:55 +0300

    i18n added.

---
+++ src/assets/i18n/internalization.vue
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+
+Vue.use(VueI18n)
+
+export default {
+  const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界'
+    }
+  }
+}
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'ja', // set locale
+  messages, // set locale messages
+})
+
+
+// Create a Vue instance with `i18n` option
+new Vue({ i18n }).$mount('#app')
+
+}
