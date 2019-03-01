f403a31bbea85c87ebe454e6e3d09594ae29ca61
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-01 22:44:20 +0300

    App updated.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view/>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'label-box',
-  };
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      taglist: [ /* {label: "label", class: "className", xmin: 0, ymin: 0, xmax: 100, ymax: 100, mode: 'create'} */ ]
+    }
+  },
+  state: {
+    taglist: [ /* {label: "label", class: "className", xmin: 0, ymin: 0, xmax: 100, ymax: 100, mode: 'create'} */ ]
+  }
+}
 </script>
 
 <style>
-  /* CSS */
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
