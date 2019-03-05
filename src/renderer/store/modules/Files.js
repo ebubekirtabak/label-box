b7598c7a654402ac60a7af8ac7ecf5c470942914
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-05 20:27:15 +0300

    indentation fix.

--- src/renderer/store/modules/Files.js
+++ src/renderer/store/modules/Files.js
@@ -8,8 +8,14 @@ const mutations = {
   },
 };
 
+const actions = {
+  setFolderGroups ({ commit }) {
+    commit('SET_FOLDER_GROUPS');
+  },
+};
 
 export default {
   state,
+  actions,
   mutations,
 };
