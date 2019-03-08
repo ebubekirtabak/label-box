0fe29f35fef73bf1c11de561ad0ab74c406f49e3
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-08 19:39:48 +0300

    Auto stash before merge of "master" and "origin/master"

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,24 @@
 // vue.config.js
 module.exports = {
-  chainWebpack: (config) => {
+  chainWebpack: (api, config) => {
     config.module
       .rule('vue')
       .use('vue-loader')
       .loader('vue-loader')
-      .tap((options) => { return options; });
+      .alias
+      .set('@', api.resolve('src'))
+      .tap(options => options);
+
+    config.resolve
+      .set('symlinks', false)
+      .extensions
+      .merge(['.js', '.jsx', '.vue', '.json'])
+      .end()
+      .modules
+      .add('node_modules')
+      .add(api.resolve('node_modules'))
+      .end()
+      .alias
+      .set('@', api.resolve('src'));
   },
 };
