223cafd9538734c5c22bd53c5ded51f1fde56d80
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-08 22:19:01 +0300

    vue cli updated.

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/home/Home';
+import HomeComponent from '@/components/home/HomeComponent';
 
 Vue.use(Router);
 
@@ -18,7 +18,7 @@ export default new Router({
     {
       path: '/home',
       name: 'Home',
-      component: Home,
+      component: HomeComponent,
     },
   ],
 });
