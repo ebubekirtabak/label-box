1cef1752b025ed8ff8e16a11a03425e922322e67
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-12 22:43:27 +0300

    indentation fix.

--- src/renderer/components/area/AreaComponent.vue
+++ src/renderer/components/area/AreaComponent.vue
@@ -29,26 +29,26 @@
         </div>
         <!-- image tag dialog -->
         <div class="dialog-container" v-show="selectedTag && selectedTag.mode === 'edit'">
-            <div id="tag_dialog" class="tag-dialog" >
-                <div class="tag-dialog__body">
-                    <div class="tag-dialog__body__form-content">
-                        <label>Object Name</label>
-                        <input ref="tag_name" id="imput_tag_name" type="text" v-model="selectedTag.label"  autofocus v-on:keyup.13="dismissDialog()"/>
-                    </div>
-                    <div class="tag-dialog__body__form-content">
-                        <label>Class Name</label>
-                        <input id="imput_class_name" type="text"  v-model="selectedTag.class"  v-on:keyup.13="dismissDialog()"/>
-                    </div>
-                </div>
-                <div class="tag-dialog__footer">
-                    <a class="tag-dialog__footer-btn left flex-item" v-on:click="dismissDialog()">
-                        <i class="fas fa-check"></i> ok
-                    </a>
-                    <a class="tag-dialog__footer-btn right flex-item">
-                        <i class="fas fa-times"></i> remove
-                    </a>
-                </div>
+          <div id="tag_dialog" class="tag-dialog" >
+            <div class="tag-dialog__body">
+              <div class="tag-dialog__body__form-content">
+                <label>Object Name</label>
+                <input ref="tag_name" id="imput_tag_name" type="text" v-model="selectedTag.label"  autofocus v-on:keyup.13="dismissDialog()"/>
+              </div>
+              <div class="tag-dialog__body__form-content">
+                <label>Class Name</label>
+                <input id="imput_class_name" type="text"  v-model="selectedTag.class"  v-on:keyup.13="dismissDialog()"/>
+              </div>
             </div>
+            <div class="tag-dialog__footer">
+              <a class="tag-dialog__footer-btn left flex-item" v-on:click="dismissDialog()">
+                <i class="fas fa-check"></i> ok
+              </a>
+              <a class="tag-dialog__footer-btn right flex-item">
+                <i class="fas fa-times"></i> remove
+              </a>
+            </div>
+          </div>
         </div>
         <!-- image tag dialog -->
         <div class="main-area__image__info">
@@ -74,7 +74,7 @@
 
       },
       dismissDialog () {
-        this.selectedTag = {};
+        this.selectedTag = { label: '' };
         this.isSelectedTag = false;
       },
       loadedImage () {
@@ -97,7 +97,7 @@
           console.log(`Mode none: ${ this.selectedTag.mode }`);
           this.selectedTag.mode = 'none';
           this.isSelectedTag = false;
-          this.selectedTag = {};
+          this.selectedTag = { label: '' };
         } else if (this.selectedTag.mode === 'create') {
           this.selectedTag.mode = 'edit';
           this.openEditMode(this.selectedTag);
@@ -126,7 +126,7 @@
           setTimeout(() => {
             this.selectedTag = 'none';
             this.isSelectedTag = false;
-            this.selectedTag = {};
+            this.selectedTag = { label: '' };
           }, 500);
         }
       },
@@ -145,7 +145,7 @@
         console.log('mouseLeave ------');
         if (element.mode !== 'create' && this.selectedTag.mode !== 'edit') {
           this.selectedTag.mode = 'none';
-          this.selectedTag = {};
+          this.selectedTag = { label: '' };
         }
       },
       onMouseClick (event) {
