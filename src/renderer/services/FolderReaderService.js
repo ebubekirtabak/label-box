2952dfd2cb5f8987d951ca0652d4b0067435f54c
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-18 21:58:51 +0300

    readAllDiles and readFile Methods added.

--- src/renderer/services/FolderReaderService.js
+++ src/renderer/services/FolderReaderService.js
@@ -18,7 +18,23 @@ export default class ForderReaderService {
     return files;
   }
 
-  readAllTextFiles(files) {
+  readAllTextFiles (files) {
+    const fileTexts = {};
+    files.filter(file => file.type.indexOf('text') !== -1).forEach((file) => {
+      fileTexts[file.path] = this.readFile(file);
+    });
+    console.log(fileTexts);
+  }
 
+  readFile (file) {
+    return new Promise((resolve, error) => {
+      fs.readFile(file.path || file, (err, data) => {
+        if (err) {
+          error(err);
+        }
+
+        resolve(data.toString());
+      });
+    });
   }
 }
