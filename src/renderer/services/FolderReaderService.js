f81b3cb5c027032b7eab29be13c55df55af45566
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-22 08:48:59 +0300

    class name fix.

--- src/renderer/services/FolderReaderService.js
+++ src/renderer/services/FolderReaderService.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const mime = require('mime');
 
-export default class ForderReaderService {
+export default class FolderReaderService {
   getAllFiles (folders) {
     const folderGroups = {};
     folders.forEach((path) => {
