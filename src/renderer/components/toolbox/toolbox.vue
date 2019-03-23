0eb9f6af618cc44e67249371ac2c71eada7ed6d2
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-23 23:54:45 +0300

    added toolbox script.

---
+++ src/renderer/components/toolbox/toolbox.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="toolbox">
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+      name: 'ToolBox',
+      data () {
+        return {
+          imagesArray: [],
+          xmlFiles: [],
+        };
+      },
+      methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
