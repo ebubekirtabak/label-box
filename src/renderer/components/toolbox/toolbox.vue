88bd470b254cf32b2f3c79cd37c68590225271fa
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-27 22:15:40 +0300

    indentation fix.

--- src/renderer/components/toolbox/toolbox.vue
+++ src/renderer/components/toolbox/toolbox.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="toolbox">
-
+      <div class="toolbox__list">
+        <div class="toolbox__list__item">
+        </div>
+        <div class="toolbox__list__item">
+        </div>
+        <div class="toolbox__list__item">
+        </div>
+      </div>
     </div>
 </template>
 
@@ -23,14 +30,15 @@
 
 <style lang="scss" scoped>
   .toolbox {
-      max-width: 128px;
-      max-height: 580px;
-      &__list {
-        width: 100%;
-        padding: 10px 5px;
-        &__item {
-
-        }
+    max-width: 128px;
+    max-height: 580px;
+    &__list {
+      width: 100%;
+      padding: 10px 5px;
+      &__item {
+        width: 72px;
+        height: 72px;
       }
+    }
   }
 </style>
