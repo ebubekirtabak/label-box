79527b113aecdbbfcf2f6b8f075e48e524474c61
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-30 22:56:15 +0300

    added mouse grid component.

---
+++ src/renderer/components/mousegrid/MouseGridComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mouse-grid">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'MouseGrid',
+    data () {
+      return {
+      };
+    },
+    methods: {
+
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+    .mouse-grid {
+
+    }
+</style>
