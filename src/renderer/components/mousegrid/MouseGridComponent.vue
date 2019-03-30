ac58cd3945a509ee192114055d400667fefb653d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-03-30 23:36:04 +0300

    added mousegrid.

--- src/renderer/components/mousegrid/MouseGridComponent.vue
+++ src/renderer/components/mousegrid/MouseGridComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mouse-grid">
-        <div class="mouse-grid__vertical-line"></div>
-        <div class="mouse-grid__horizontal-line"></div>
+        <div class="mouse-grid__vertical-line" v-bind:style="{ left: this.mouse.x + 'px' }"></div>
+        <div class="mouse-grid__horizontal-line" v-bind:style="{ top: this.mouse.y + 'px' }"></div>
     </div>
 </template>
 
@@ -15,6 +15,11 @@
     methods: {
 
     },
+    computed: {
+      mouse () {
+        return this.$store.getters.getMouse;
+      },
+    },
   };
 </script>
 
@@ -24,7 +29,17 @@
         width: 100%;
         height: 100%;
         &__vertical-line {
+            position: absolute;
+            width: 1px;
+            background: #000000;
+            height: 100%;
+        }
 
+        &__horizontal-line {
+            position: absolute;
+            height: 1px;
+            background: #000000;
+            width: 100%;
         }
     }
 </style>
