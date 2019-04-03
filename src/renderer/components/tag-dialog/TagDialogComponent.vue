7623991d0a4068912b7aa0ef10e467b94c3a58bd
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-04-03 21:22:39 +0300

    Added tag dialog component class.

---
+++ src/renderer/components/tag-dialog/TagDialogComponent.vue
@@ -0,0 +1,46 @@
+<template>
+
+</template>
+<div class="dialog-container" v-show="selectedTag && selectedTag.mode === 'edit'">
+    <div id="tag_dialog" class="tag-dialog" >
+        <div class="tag-dialog__body">
+            <div class="tag-dialog__body__form-content">
+                <label>Object Name</label>
+                <input ref="tag_name" id="imput_tag_name" type="text" v-model="selectedTag.label"  autofocus v-on:keyup.13="dismissDialog()"/>
+            </div>
+            <div class="tag-dialog__body__form-content">
+                <label>Class Name</label>
+                <input id="imput_class_name" type="text"  v-model="selectedTag.class"  v-on:keyup.13="dismissDialog()"/>
+            </div>
+        </div>
+        <div class="tag-dialog__footer">
+            <a class="tag-dialog__footer-btn left flex-item" v-on:click="dismissDialog()">
+                <i class="fas fa-check"></i> ok
+            </a>
+            <a class="tag-dialog__footer-btn right flex-item">
+                <i class="fas fa-times"></i> remove
+            </a>
+        </div>
+    </div>
+</div>
+
+<script>
+  export default {
+    name: 'TagDialogComponent',
+    data () {
+      return {
+        isShow: false,
+      };
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
