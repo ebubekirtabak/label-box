26a1a3f9498e6dec09eeb67db47630abe4db4648
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-21 10:54:51 +0300

    indent fix.

--- src/renderer/components/mousegrid/MouseGridComponent.vue
+++ src/renderer/components/mousegrid/MouseGridComponent.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="mouse-grid" v-show="isShow">
-        <div class="mouse-grid__vertical-line" v-bind:style="{ left: this.mouse.x + 'px' }"></div>
-        <div class="mouse-grid__horizontal-line" v-bind:style="{ top: this.mouse.y + 'px' }"></div>
-    </div>
+  <div class="mouse-grid" v-show="isShow">
+    <div class="mouse-grid__vertical-line" v-bind:style="{ left: this.mouse.x + 'px' }"></div>
+    <div class="mouse-grid__horizontal-line" v-bind:style="{ top: this.mouse.y + 'px' }"></div>
+  </div>
 </template>
 
 <script>
@@ -25,22 +25,22 @@
 </script>
 
 <style lang="scss" scoped>
-    .mouse-grid {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        &__vertical-line {
-            position: absolute;
-            width: 1px;
-            background: #000000;
-            height: 100%;
-        }
-
-        &__horizontal-line {
-            position: absolute;
-            height: 1px;
-            background: #000000;
-            width: 100%;
-        }
+  .mouse-grid {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    &__vertical-line {
+      position: absolute;
+      width: 1px;
+      background: #000000;
+      height: 100%;
     }
+
+    &__horizontal-line {
+      position: absolute;
+      height: 1px;
+      background: #000000;
+      width: 100%;
+    }
+  }
 </style>
