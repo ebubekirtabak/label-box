3d5491f089cbafd8bff040debeaa90db9b1c66b3
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-22 09:37:10 +0300

    indent fix

--- src/renderer/components/home/home.vue
+++ src/renderer/components/home/home.vue
@@ -1,46 +1,46 @@
 <template>
-    <div>
-        <NavBar></NavBar>
-        <div class="main-area">
-            <div class="main-area__layer">
-                <div class="main-area__layer__list">
-                    <div class="main-area__layer__list__item" v-for="(item, index) in this.selectedImage.tagList" :key='index'>
-                        <div class="main-area__layer__list__item-title"> {{ item.label }}</div>
-                        <div class="main-area__layer__list__item-text"> {{ item.class }} </div>
-                        <div class="main-area__layer__list__item-detail">
-                            <div class="main-area__layer__list__item-detail-item">
-                                WIDTH: {{ item.width }}
-                            </div>
-                            <div class="main-area__layer__list__item-detail-item">
-                                HEIGHT: {{ item.height }}
-                            </div>
-                            <div class="main-area__layer__list__item-detail-item">
-                                X: {{ item.xmin}}
-                            </div>
-                            <div class="main-area__layer__list__item-detail-item">
-                                Y: {{ item.ymin }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <div>
+    <NavBar></NavBar>
+    <div class="main-area">
+      <div class="main-area__layer">
+        <div class="main-area__layer__list">
+          <div class="main-area__layer__list__item" v-for="(item, index) in this.selectedImage.tagList" :key='index'>
+            <div class="main-area__layer__list__item-title"> {{ item.label }}</div>
+            <div class="main-area__layer__list__item-text"> {{ item.class }} </div>
+            <div class="main-area__layer__list__item-detail">
+              <div class="main-area__layer__list__item-detail-item">
+                WIDTH: {{ item.width }}
+              </div>
+              <div class="main-area__layer__list__item-detail-item">
+                HEIGHT: {{ item.height }}
+              </div>
+              <div class="main-area__layer__list__item-detail-item">
+                X: {{ item.xmin}}
+              </div>
+              <div class="main-area__layer__list__item-detail-item">
+                Y: {{ item.ymin }}
+              </div>
             </div>
-            <AreaComponent></AreaComponent>
-            <div class="main-area__layer">
-                <div class="main-area__layer__list">
-                    <div class="main-area__layer__list__image-item"  v-for="(image, index) in imageList" :key='index' v-on:click="selectImage(image)">
-                        <div class="main-area__layer__list__image-item--overlay overlay-item"></div>
-                        <div class="main-area__layer__list__image-item--check " v-bind:class="{'check-item': image.tagList.length > 0}">
-                            <img src="@/assets/img/verified.png">
-                        </div>
-                        <div class="main-area__layer__list__image-item__footer">
-                            {{ image.name }}
-                        </div>
-                        <img :src="'file://' + image.fullPath" height="auto"/>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
+      <AreaComponent></AreaComponent>
+      <div class="main-area__layer">
+        <div class="main-area__layer__list">
+          <div class="main-area__layer__list__image-item"  v-for="(image, index) in imageList" :key='index' v-on:click="selectImage(image)">
+            <div class="main-area__layer__list__image-item--overlay overlay-item"></div>
+            <div class="main-area__layer__list__image-item--check " v-bind:class="{'check-item': image.tagList.length > 0}">
+              <img src="@/assets/img/verified.png">
+            </div>
+            <div class="main-area__layer__list__image-item__footer">
+              {{ image.name }}
+            </div>
+            <img :src="'file://' + image.fullPath" height="auto"/>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
